9b8b5433a431b239382d4d03eb6f1d4a253ac0a6
zhanglan <delylaric@gmail.com>
2018-08-09 10:46:09 +0800

    add: vuex preparation

--- src/core/vue.js
+++ src/core/vue.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import router from './router'
+import store from './vuex'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h('router-view'),
-  router
+  router,
+  store
 }).$mount('#app')
